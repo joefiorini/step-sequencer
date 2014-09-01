a02949d7ee9024f4923ce8fac064ca176e1a65df
Joe Fiorini <joe@joefiorini.com>
2014-08-31 23:22:06 -0400

    Add play/reset buttons

--- src/step_sequencer/core.cljs
+++ src/step_sequencer/core.cljs
@@ -6,10 +6,27 @@
 
 (def app-state (atom {:text "Hello world!"}))
 
-(om/root
-  (fn [app owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text app)))))
-  app-state
+(defn play-state-controls [playing owner]
+  (reify
+    om/IRender
+    (render [this]
+            (dom/ul #js {:className "list-plain list-horizontal"}
+                    (dom/li nil
+                            (cond
+                             playing (dom/button #js {:onClick #(om/set-state! owner :playing false)} "Stop")
+                             :else (dom/button #js {:onClick #(om/set-state! owner :playing true)} "Play")))
+                    (dom/li nil (dom/button nil "Reset"))
+                    ))))
+
+(defn sequencer-view [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+                {:playing false})
+    om/IRenderState
+    (render-state [_ {:keys [playing]}]
+            (dom/div #js {:className "sequencer"}
+                     (om/build play-state-controls playing)))))
+
+(om/root sequencer-view app-state
   {:target (. js/document (getElementById "app"))})
